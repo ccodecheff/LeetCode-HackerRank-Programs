28a1a5e5548dbaee9d231d19a00edd9fa6ddc09b
ccodecheff <rathore.karan.sn1@gmail.com>
2021-02-08 20:27:28 -0800

    remove duplicate example leetcode

---
+++ removeDuplicateFromArray.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// This function will return the length of unique values in an array
+var removeDuplicates = function(nums) {
+    if (!nums.length) {
+    return 0;
+  }
+
+  let i = 0;
+  for (let j = 1;j < nums.length;j++) {
+    if (nums[i] !== nums[j]) {
+        i++;
+      nums[i] = nums[j];
+      console.log("num of index"+i+" "+"value:"+nums[i]);
+    }
+  }
+console.log(nums);
+  return i + 1;
+
+};
+
+console.log(removeDuplicates([1,1,1,2,3,4,4]));
